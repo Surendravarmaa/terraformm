38b8bc60458363d2062c848f1f1c6b44340ddbdc
unknown <surendravarmajampana.59@gmail.com>
2026-04-03 18:37:07 +0530

    terraform

---
+++ ec2/ec2.tf
@@ -0,0 +1,25 @@
+# resource <resource_type> <resource_name>
+resource "aws_security_group" "allow_ssh" {
+    name = "allow_ssh"
+    description = "allowing SSH access"
+
+    ingress {
+        from_port        = 22
+        to_port          = 22
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    egress {
+        from_port        = 0 # from 0 to 0 means, opening all protocols
+        to_port          = 0
+        protocol         = "-1" # -1 all protocols
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "allow_ssh"
+        CreatedBy = "Surendra"
+    }
+}
