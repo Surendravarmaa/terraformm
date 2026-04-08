121eb3f76066473c4df427358776b41b8bf95692
unknown <surendravarmajampana.59@gmail.com>
2026-04-08 21:51:08 +0530

    terraform

---
+++ dynamic/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.39.0"
+    }
+  }
+  backend "s3" {
+    bucket = "surendra-remote-state"
+    key    = "dynamic-block"
+    region = "us-east-1"
+    dynamodb_table = "surendra-locking"
+  }
+}
+
+#provide authentication here
+provider "aws" {
+    region = "us-east-1"
+}
